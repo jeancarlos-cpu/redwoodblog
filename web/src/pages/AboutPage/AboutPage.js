1a987e9012f64716c521678d53ffb1c2b1b8707d
Your Name <you@example.com>
2020-06-14 14:11:57 -0300

    posts

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,16 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const AboutPage = () => {
+  return (
+    <BlogLayout>
+      <main>
+        <p>
+          This site was created to demonstrate my mastery of Redwood: Look on my
+          works, ye mighty, and despair!
+        </p>
+      </main>
+    </BlogLayout>
+  )
+}
+
+export default AboutPage
